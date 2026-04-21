6a175372acadf77d9526483160b892618b036134
Mario Dominguez <mario.dominguez@youforce.net>
2026-04-21 15:47:42 +0200

    Redesign compound interest quick insights

--- src/app/pages/simulations/compound-interest/presentation/components/insights-card/insights-card.component.ts
+++ src/app/pages/simulations/compound-interest/presentation/components/insights-card/insights-card.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { ComponentCardComponent } from '../../../../../../shared/components/common/component-card/component-card.component';
 import { SimulationSummary } from '../../../domain/models/compound-interest.models';
 import { formatCurrency, formatPercent } from '../../utils/compound-interest-formatters';
 
 @Component({
   selector: 'app-insights-card',
   host: { class: 'block' },
-  imports: [ComponentCardComponent],
+  imports: [],
   templateUrl: './insights-card.component.html',
 })
 export class InsightsCardComponent {
