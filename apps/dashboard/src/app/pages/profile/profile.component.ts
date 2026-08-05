ee45bee97072442e8198b130c46868085e37776b
Mario Dominguez <opeshm@gmail.com>
2026-08-05 20:18:42 +0200

    style(profile): remove user subtitle, address section, phone and bio fields

--- apps/dashboard/src/app/pages/profile/profile.component.ts
+++ apps/dashboard/src/app/pages/profile/profile.component.ts
@@ -3,19 +3,18 @@ import { Component } from '@angular/core';
 import { PageBreadcrumbComponent } from '../../shared/components/common/page-breadcrumb/page-breadcrumb.component';
 import { UserMetaCardComponent } from '../../shared/components/user-profile/user-meta-card/user-meta-card.component';
 import { UserInfoCardComponent } from '../../shared/components/user-profile/user-info-card/user-info-card.component';
-import { UserAddressCardComponent } from '../../shared/components/user-profile/user-address-card/user-address-card.component';
 
 @Component({
   selector: 'app-profile',
   imports: [
     PageBreadcrumbComponent,
     UserMetaCardComponent,
-    UserInfoCardComponent,
-    UserAddressCardComponent
-],
+    UserInfoCardComponent
+  ],
   templateUrl: './profile.component.html',
   styles: ``
 })
 export class ProfileComponent {
 
 }
+
